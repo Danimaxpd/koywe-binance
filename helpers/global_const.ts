f590e90967badbb40d523cc2effec6db495eb07a
David Jm <danimax.com@gmail.com>
2023-08-13 00:23:29 -0500

    Updated Global const

--- helpers/global_const.ts
+++ helpers/global_const.ts
@@ -1,8 +1,4 @@
 require("dotenv").config();
 
-const BINANCE_URL_DOMAIN: string =
+export const BINANCE_URL_DOMAIN: string =
   process.env.NEXT_PUBLIC_URL_BASE_PROJECT || "https://testnet.binance.vision";
-
-module.exports = {
-  BINANCE_URL_DOMAIN,
-};
