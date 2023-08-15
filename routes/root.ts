a720216669cfee7f4a8d29f27d55c608e1f3ebe7
David Jimenez <danimax.com@gmail.com>
2023-08-15 00:48:46 -0500

    Updated root routes

--- routes/root.ts
+++ routes/root.ts
@@ -1,9 +1,13 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from "fastify";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    return { root: true };
+  });
+
+  fastify.get("/ping", async function (request, reply) {
+    return { pong: true };
+  });
+};
 
 export default root;
